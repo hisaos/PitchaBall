891e2efd501e6f5f89897902badcf2a5f0a8dd9c
Hisao Setoguchi <setoguchi@hisaos.com>
2021-05-04 21:01:21 +0900

    Test: ボールを一定間隔でぽろぽろ

--- Assets/Scripts/Test/Pitcher.cs
+++ Assets/Scripts/Test/Pitcher.cs
@@ -8,12 +8,24 @@ namespace Test
   public class Pitcher : MonoBehaviour
   {
     public GameObject ball;
+    private float _pitchInterval;
+    public float pitchInterval = 0.5f;
+
+    void Start()
+    {
+      _pitchInterval = pitchInterval;
+    }
 
     // Update is called once per frame
     void Update()
     {
+      _pitchInterval -= Time.deltaTime;
+      if (_pitchInterval < 0f)
+      {
         var b = Instantiate(ball);
-        b.transform.localPosition = this.transform.position + Vector3.forward;        
+        b.transform.localPosition = this.transform.position + Vector3.forward;
+        _pitchInterval = pitchInterval;
+      }
     }
   }
 }
