2ffa1b920aaf6f6dfa6987ee040acfadc67de220
Hisao Setoguchi <setoguchi@hisaos.com>
2021-05-04 20:58:55 +0900

    Test: ボールだばだば

---
+++ Assets/Scripts/Test/Pitcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+
+{
+  public class Pitcher : MonoBehaviour
+  {
+    public GameObject ball;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var b = Instantiate(ball);
+        b.transform.localPosition = this.transform.position + Vector3.forward;        
+    }
+  }
+}
