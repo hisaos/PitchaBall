84e2e386afeaadc123ed35af6c0f2db5b5814f0d
Hisao Setoguchi <setoguchi@hisaos.com>
2021-05-04 23:19:35 +0900

    Test: バットに当たったボールに適当な力を加えて飛ばす

---
+++ Assets/Scripts/Test/Bat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+  public class Bat : MonoBehaviour
+  {
+    void OnCollisionEnter(Collision other)
+    {
+      if (other.gameObject.CompareTag("Ball"))
+      {
+        var r = other.gameObject.GetComponent<Rigidbody>();
+        r.AddForce((r.transform.position - other.GetContact(0).point).normalized * 2f, ForceMode.Impulse);
+      }
+    }
+  }
+}
