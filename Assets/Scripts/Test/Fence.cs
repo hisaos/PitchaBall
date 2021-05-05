d5db630eba741a18278a41dc4d259071a08b2417
Hisao Setoguchi <setoguchi@hisaos.com>
2021-05-05 22:13:11 +0900

    Test: メッセージシステムでPitcherの状態制御する関数を呼び出すようにした

---
+++ Assets/Scripts/Test/Fence.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Test
+{
+  public class Fence : MonoBehaviour
+  {
+    private GameObject _pitcher;
+
+    void Start()
+    {
+      _pitcher = FindObjectOfType<Pitcher>().gameObject;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+      if (other.gameObject.CompareTag("Ball"))
+      {
+        ExecuteEvents.Execute<ICustomMessageTarget>(
+            target: _pitcher,
+            eventData: null,
+            functor: (receiver, eventData) => receiver.EnablePitch()
+        );
+      }
+    }
+  }
+}
--- Assets/Scripts/Test/Pitcher.cs
+++ Assets/Scripts/Test/Pitcher.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Test
 
 {
-  public class Pitcher : MonoBehaviour
+  public class Pitcher : MonoBehaviour, ICustomMessageTarget
   {
     public GameObject ball;
     private float _pitchInterval;
     public float pitchInterval = 1.5f;
     public float pitchForwardForce = 0.8f;
     public float pitchUpForce = 0.4f;
+
+    private bool _isPitched = false;
 
     void Start()
     {
@@ -21,6 +21,8 @@ namespace Test
     // Update is called once per frame
     void Update()
     {
+      if (_isPitched) return;
+
       _pitchInterval -= Time.deltaTime;
       if (_pitchInterval < 0f)
       {
@@ -29,7 +31,14 @@ namespace Test
         _pitchInterval = pitchInterval;
         var rb = b.GetComponent<Rigidbody>();
         rb.AddForce(Vector3.forward * pitchForwardForce + Vector3.up * pitchUpForce, ForceMode.Impulse);
+        _isPitched = true;
       }
+    }
+
+    public void EnablePitch()
+    {
+      Debug.Log(this.gameObject.name + "EnablePitch");
+      _isPitched = false;
     }
   }
 }
