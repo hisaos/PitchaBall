bf353b2d366e7471da9914cd51fad03b8865e0aa
Hisao Setoguchi <setoguchi@hisaos.com>
2021-05-07 23:16:51 +0900

    Test: プレイが終わるまでは次の球に行かない（バットにボールが当たったら消す処理をなくす）

--- Assets/Scripts/Test/Bat.cs
+++ Assets/Scripts/Test/Bat.cs
@@ -24,16 +24,6 @@ namespace Test
         var r = other.gameObject.GetComponent<Rigidbody>();
         r.velocity = Vector3.zero;
         r.AddForce((r.transform.position - other.GetContact(0).point).normalized * batterPower, ForceMode.Impulse);
-
-        // ボールを消す（1.5秒後）
-        Destroy(other.gameObject, 1.5f);
-
-        // ピッチャーを投げれる状態にする
-        ExecuteEvents.Execute<ICustomMessageTarget>(
-            target: _pitcher,
-            eventData: null,
-            functor: (receiver, eventData) => receiver.EnablePitch()
-        );
       }
     }
   }
