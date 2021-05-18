99d613a790cbcbc5e3effad3d71150689fccbe3a
Hisao Setoguchi <setoguchi@hisaos.com>
2021-05-18 23:29:22 +0900

    Test: 返球システム（雑）

---
+++ Assets/Scripts/Test/Catcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Test
+{
+  public class Catcher : MonoBehaviour
+  {
+    private GameObject _pitcher;
+    private List<Fielder> _fielders;
+
+    void Start()
+    {
+      _pitcher = FindObjectOfType<Pitcher>().gameObject;
+      _fielders = new List<Fielder>(FindObjectsOfType<Fielder>());
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+      // ピッチャーを投げれる状態にする
+      ExecuteEvents.Execute<IPitcherMessageHandler>(
+        target: _pitcher,
+        eventData: null,
+        functor: (receiver, eventData) => receiver.EnablePitch()
+      );
+
+      // ボールを取ったら追わずに元の位置へ
+      foreach (var f in _fielders)
+      {
+        ExecuteEvents.Execute<IFielderMessageHandler>(
+          target: f.gameObject,
+          eventData: null,
+          functor: (receiver, eventData) =>
+          {
+            receiver.ResetFielderBall();
+            receiver.DisableFielderMove();
+            receiver.ReturnToOriginalPosition();
+          }
+        );
+      }
+    }
+
+  }
+}
--- Assets/Scripts/Test/Fielder.cs
+++ Assets/Scripts/Test/Fielder.cs
@@ -7,8 +7,13 @@ namespace Test
 {
   public class Fielder : MonoBehaviour, IFielderMessageHandler
   {
+    public GameObject ball;
+    public float pitchUpForce;
+    public float pitchForwardForce;
+
     private GameObject _pitcher;
-    private GameObject _ball;
+    private GameObject _catcher;
+    private GameObject _pitchedBall;
     private bool _chaseBall;
     private Rigidbody _rb;
 
@@ -19,10 +24,15 @@ namespace Test
     private float _distToBall;
     private bool _isMinDistToBall;
 
+    private bool _pickBall;
+    private bool _isCatcher = false;
+
     // Start is called before the first frame update
     void Start()
     {
       _pitcher = FindObjectOfType<Pitcher>().gameObject;
+      _catcher = FindObjectOfType<Catcher>().gameObject;
+      if (_catcher == this.gameObject) _isCatcher = true;
       _rb = this.gameObject.GetComponent<Rigidbody>();
       _fielders = new List<Fielder>(FindObjectsOfType<Fielder>());
       originalPosition = this.transform.position;
@@ -31,10 +41,10 @@ namespace Test
     void FixedUpdate()
     {
       // ボールを追っかけさせる
-      if (!(_ball && _chaseBall)) return;
+      if (!(_pitchedBall && _chaseBall)) return;
 
       // ボールまでのx-z平面上の距離の計算
-      var ballPos = _ball.transform.position;
+      var ballPos = _pitchedBall.transform.position;
       var pos = this.transform.position;
       var distToBall = new Vector3(ballPos.x - pos.x, 0f, ballPos.z - pos.z);
       _distToBall = distToBall.magnitude;
@@ -65,37 +75,51 @@ namespace Test
       var moveDir = distToBall.normalized;
       // velocityを直接変えて一定スピードで動くようにする
       _rb.velocity = moveDir * moveSpeed;
-//      _rb.AddForce(moveDir * moveSpeed, ForceMode.VelocityChange);
+      //      _rb.AddForce(moveDir * moveSpeed, ForceMode.VelocityChange);
     }
 
     void OnCollisionEnter(Collision other)
     {
       if (other.gameObject.CompareTag("Ball"))
       {
-        // ピッチャーを投げれる状態にする
-        ExecuteEvents.Execute<IPitcherMessageHandler>(
-          target: _pitcher,
-          eventData: null,
-          functor: (receiver, eventData) => receiver.EnablePitch()
-        );
+        // ボールを拾った
+        _pickBall = true;
 
-        // ボールを取ったら追わずに元の位置へ
-        foreach (var f in _fielders)
-        {
-          ExecuteEvents.Execute<IFielderMessageHandler>(
-            target: f.gameObject,
-            eventData: null,
-            functor: (receiver, eventData) =>
-            {
-              receiver.ResetFielderBall();
-              receiver.DisableFielderMove();
-              receiver.ReturnToOriginalPosition();
-            }
-          );
-        }
+        // // ピッチャーを投げれる状態にする
+        // ExecuteEvents.Execute<IPitcherMessageHandler>(
+        //   target: _pitcher,
+        //   eventData: null,
+        //   functor: (receiver, eventData) => receiver.EnablePitch()
+        // );
+
+        // // ボールを取ったら追わずに元の位置へ
+        // foreach (var f in _fielders)
+        // {
+        //   ExecuteEvents.Execute<IFielderMessageHandler>(
+        //     target: f.gameObject,
+        //     eventData: null,
+        //     functor: (receiver, eventData) =>
+        //     {
+        //       receiver.ResetFielderBall();
+        //       receiver.DisableFielderMove();
+        //       receiver.ReturnToOriginalPosition();
+        //     }
+        //   );
+        // }
 
         // ボールを消す（即時）->捕球
         Destroy(other.gameObject);
+
+        // ボールを返球する（雑）
+        if (!_isCatcher)
+        {
+          var b = Instantiate(ball);
+          b.transform.localPosition = this.transform.position + Vector3.forward;
+          var rb = b.GetComponent<Rigidbody>();
+          var _directionToCatcher = (_catcher.transform.position - this.transform.position).normalized;
+          rb.AddForce(_directionToCatcher * pitchForwardForce + Vector3.up * pitchUpForce, ForceMode.Impulse);
+          _pickBall = false;
+        }
       }
     }
 
@@ -103,7 +127,7 @@ namespace Test
     public void SetFielderBall(GameObject ball)
     {
       Debug.Log("SetFielderBall");
-      _ball = ball;
+      _pitchedBall = ball;
     }
 
     // ボールが打たれた時のメッセージハンドラ
@@ -117,7 +141,7 @@ namespace Test
     public void ResetFielderBall()
     {
       Debug.Log("ResetFielderBall");
-      _ball = null;
+      _pitchedBall = null;
     }
 
     // 追わなくなる時のメッセージハンドラ
@@ -135,6 +159,7 @@ namespace Test
       Debug.Log("Return");
       _rb.velocity = Vector3.zero;
       this.transform.position = originalPosition;
+      _pickBall = false;
     }
 
     // ボールまでの距離を教え合うメッセージハンドラ
